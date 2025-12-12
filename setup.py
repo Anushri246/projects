6e36a880832836639af12a49c2784c81ae850325
Anushri <anushripll24@gmail.com>
2025-12-12 13:44:00 +0530

    First setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements(file_path:str)->List[str]:
+
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n',"") for req in requirements]
+
+    return requirements
+
+
+
+
+setup(
+    name = 'anushri-project',
+    version = '0.1.0',
+    author = 'Anushri',
+    author_email = 'anushripll24@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+)
